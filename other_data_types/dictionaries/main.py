4b0fcf8140a020373bc7440af7816cc66eb5aee7
Jmontano22 <>
2025-11-30 02:22:07 +0000

    Dictionaries

---
+++ other_data_types/dictionaries/main.py
@@ -0,0 +1,10 @@
+# Define a dictionary
+grocery_inventory = {
+    "Milk": (113, "Dairy"),
+    "Eggs": (116, "Dairy"),
+    "Bread": (117, "Bakery"),
+    "Apples": (141, "Produce")
+}
+bread_details = grocery_inventory["Bread"]
+print("Bread:", bread_details)
+
